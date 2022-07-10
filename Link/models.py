76796790bab7b497aa936317d4914ce6d0af9ce9
SaniKyn <gajdukevich.sani@mail.ru>
2022-07-10 11:56:17 +0300

    Creating_a_Database_Models

--- Link/models.py
+++ Link/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Question(models.Model):
+    original_url = models.CharField(max_length=256)
+    hash = models.CharField(max_length=10)
+    creation_date = models.DateTimeField('creation date')
--- Link/views.py
+++ Link/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import service
 from django.http import HttpResponse
+from django.urls import reverse
 import pyshorteners
 
+
 def shorten(request, url):
-    shortener = pyshorteners.Shortener()
-    shortened_url = shortener.chilpit.short(url)
+    shortened_url_hash = service.shorten(url)
+    shortened_url = request.build_absolute_uri(reverse('redirect', args=[shortened_url_hash]))
     return HttpResponse(f'Shortened URL: <a href="{shortened_url}">{shortened_url}</a>')
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def shorten_post(request):
+    return shorten(request, request.POST['url'])
+
+
+def redirect_hash(request, url_hash):
+    original_url = service.load_url(url_hash).original_url
+    return redirect(original_url)
+
