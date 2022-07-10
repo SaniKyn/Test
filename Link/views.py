a296e4918ba1458334f5b0f90a0cab4670d90dde
SaniKyn <gajdukevich.sani@mail.ru>
2022-07-10 11:43:26 +0300

    Create_the_URL_Shortener

---
+++ Link/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import pyshorteners
+
+def shorten(request, url):
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.chilpit.short(url)
+    return HttpResponse(f'Shortened URL: <a href="{shortened_url}">{shortened_url}</a>')
