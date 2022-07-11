768922ead8381c139ea22de9e52303e57db1b577
SaniKyn <gajdukevich.sani@mail.ru>
2022-07-11 14:02:47 +0300

    Base_update

--- Link/models.py
+++ Link/models.py
@@ -5,3 +5,7 @@ class LinkMapping(models.Model):
     original_url = models.CharField(max_length=256)
     hash = models.CharField(max_length=10, unique=True, db_index=True)
     creation_date = models.DateTimeField('creation date')
+
+
+
+
--- Link/views.py
+++ Link/views.py
@@ -3,22 +3,20 @@ from . import service
 from django.urls import reverse
 
 
-
-def shorten(request, url):
-    shortened_url_hash = service.shorten(url)
-    shortened_url = request.build_absolute_uri(reverse('redirect', args=[shortened_url_hash]))
-    return render(request, 'link.html', {'shortened_url': shortened_url})
-
-
 def index(request):
     return render(request, 'index.html')
-
-
-def shorten_post(request):
-    return shorten(request, request.POST['url'])
 
 
 def redirect_hash(request, url_hash):
     original_url = service.load_url(url_hash).original_url
     return redirect(original_url)
 
+
+def shorten_post(request):
+    return shorten(request, request.POST['url'])
+
+
+def shorten(request, url):
+    shortened_url_hash = service.shorten(url)
+    shortened_url = request.build_absolute_uri(reverse('redirect', args=[shortened_url_hash]))
+    return render(request, 'link.html', {'shortened_url': shortened_url})
