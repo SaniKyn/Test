8e9f1e63bd92a5132950456fcb421a508015c7ed
SaniKyn <gajdukevich.sani@mail.ru>
2022-07-12 14:26:01 +0300

    use_django_shortcuts_import_get_object_or_404

--- Link/service.py
+++ Link/service.py
@@ -1,16 +1,17 @@
 import random
 import string
 from django.utils import timezone
-
+from django.shortcuts import get_object_or_404
 from .models import LinkMapping
 
 
 def shorten(url):
-    random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
+    random_hash = ''.join(
+        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
     mapping = LinkMapping(original_url=url, hash=random_hash, creation_date=timezone.now())
     mapping.save()
     return random_hash
 
 
 def load_url(url_hash):
-    return LinkMapping.objects.get(hash=url_hash)
+    return get_object_or_404(LinkMapping, hash=url_hash)
--- Link/views.py
+++ Link/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . import service
+from Link import service
 from django.urls import reverse
 
 
